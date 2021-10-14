0e747e8b22f0f3a29b644b148ef6571ac4fe91d8
Nikita_Mogrun <kudriashovns@gmail.com>
2021-10-14 23:33:26 +0600

    try add form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('Привет, Мир!')
+var Name = document.getElementsByName('user_name');
+var msg = document.getElementsByName('user_message');
+console.log("Имя: ", Name)
+console.log("Сообщение: ", msg)
